aee145c811a8ffb4b3f039ed0a16ae3e3ee7d7a5
Eliot REINSBACH <eliot.reinsbach@edu.devinci.fr>
2023-12-16 19:32:17 +0100

    debut user

---
+++ .history/API_projet_SE/API/models_20231216183838.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Musique(models.Model):
+    titre = models.CharField(max_length=100)
+    artiste = models.CharField(max_length=100)
+    album = models.CharField(max_length=100)
+    duree = models.DurationField()
+    fichier_audio = models.FileField(upload_to='musiques/')
+    auteur = models.ForeignKey(User, on_delete = models.CASCADE)

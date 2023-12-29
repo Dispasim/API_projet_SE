c7042604daf28953e432ba7e336fc88831621d38
Dispasim <91202903+Dispasim@users.noreply.github.com>
2023-12-29 16:37:41 +0100

    ze

--- API_projet_SE/API_projet_SE/urls.py
+++ API_projet_SE/API_projet_SE/urls.py
@@ -19,8 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include("API.urls", namespace = "API")),
-    path("", include("app.urls", namespace = "app")),
+    path("", include("API.urls", namespace = "API")),
+    #path("", include("app.urls", namespace = "app")),
     
     
 ]
